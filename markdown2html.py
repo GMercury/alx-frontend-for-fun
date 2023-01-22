6ffbeafbc22e796a79c67366dee3dd2d8e6f8563
GMercury <99487197+GMercury@users.noreply.github.com>
2023-01-22 12:05:02 +0300

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,96 +1,66 @@
 #!/usr/bin/python3
-
-"""
-Markdown script using python.
-"""
 import sys
-import os.path
-import re
-import hashlib
+from os.path import exists
+
+"""A markdown to html file
+    Args:
+        Arg 1: Markdown file
+        Arg 2: output file name (HTML)
+    """
+
+markdownHeader = {'#': '<h1> </h1>', '##': '<h2> </h2>', '###': '<h3> </h3>',
+                  '####': '<h4> </h4>', '#####': '<h5> </h5>', '######': '<h6> </h6>'}
+
+markdownList = {'-': '<li> </li>', '*': '<li> </li>'}
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
+
+    """Check if number of arguments == 2"""
+
+    if len(sys.argv) != 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
 
-    if not os.path.isfile(sys.argv[1]):
-        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+    """Check if input file is a correct markdown file"""
+    if "." in sys.argv[1]:
+        newArr = sys.argv[1].split('.')
+        if len(newArr) != 2:
+            sys.stderr.write('Bad Markdown file\n')
+            exit(1)
+        if newArr[1] != "md":
+            sys.stderr.write('First argument must a markdown file\n')
+
+    """Check if markdown file exist"""
+    if exists(sys.argv[1]) == False:
+        sys.stderr.write('Missing {}\n'.format(sys.argv[1]))
         exit(1)
 
-    with open(sys.argv[1]) as read:
-        with open(sys.argv[2], 'w') as html:
-            unordered_start, ordered_start, paragraph = False, False, False
-            # bold syntax
-            for line in read:
-                line = line.replace('**', '<b>', 1)
-                line = line.replace('**', '</b>', 1)
-                line = line.replace('__', '<em>', 1)
-                line = line.replace('__', '</em>', 1)
+    """Opening the markdown file for file operations"""
+    ulCount = 0
+    with open(sys.argv[1]) as markdown:
+        line = True
 
-                # md5
-                md5 = re.findall(r'\[\[.+?\]\]', line)
-                md5_inside = re.findall(r'\[\[(.+?)\]\]', line)
-                if md5:
-                    line = line.replace(md5[0], hashlib.md5(
-                        md5_inside[0].encode()).hexdigest())
+        while line:
+            line = markdown.readline()
+            if line.startswith('#'):  # Headings operation
+                hash = line.split(' ')[0]
 
-                # remove the letter C
-                remove_letter_c = re.findall(r'\(\(.+?\)\)', line)
-                remove_c_more = re.findall(r'\(\((.+?)\)\)', line)
-                if remove_letter_c:
-                    remove_c_more = ''.join(
-                        c for c in remove_c_more[0] if c not in 'Cc')
-                    line = line.replace(remove_letter_c[0], remove_c_more)
+                with open(sys.argv[2], 'a') as htmlFile:
+                    hashL = len(hash) + 1
+                    htmlFile.write('{}{}{}\n'.format(
+                        markdownHeader[hash].split(' ')[0], line[hashL: -1], markdownHeader[hash].split(' ')[1]))
 
-                length = len(line)
-                headings = line.lstrip('#')
-                heading_num = length - len(headings)
-                unordered = line.lstrip('-')
-                unordered_num = length - len(unordered)
-                ordered = line.lstrip('*')
-                ordered_num = length - len(ordered)
-                # headings, lists
-                if 1 <= heading_num <= 6:
-                    line = '<h{}>'.format(
-                        heading_num) + headings.strip() + '</h{}>\n'.format(
-                        heading_num)
+            if line.startswith('-'):  # Unordered list operation
+                with open(sys.argv[2], 'a') as htmlFile:
+                    if ulCount == 0:
+                        htmlFile.write('<ul>\n')
+                    else:
+                        htmlFile.write('\t{}{}{}\n'.format(
+                            markdownList['-'].split(' ')[0], line[2: -1], markdownList['-'].split(' ')[1]))
+                    ulCount += 1
 
-                if unordered_num:
-                    if not unordered_start:
-                        html.write('<ul>\n')
-                        unordered_start = True
-                    line = '<li>' + unordered.strip() + '</li>\n'
-                if unordered_start and not unordered_num:
-                    html.write('</ul>\n')
-                    unordered_start = False
+            else:
+                with open(sys.argv[2], 'a') as htmlFile:
+                    htmlFile.write(line)
 
-                if ordered_num:
-                    if not ordered_start:
-                        html.write('<ol>\n')
-                        ordered_start = True
-                    line = '<li>' + ordered.strip() + '</li>\n'
-                if ordered_start and not ordered_num:
-                    html.write('</ol>\n')
-                    ordered_start = False
-
-                if not (heading_num or unordered_start or ordered_start):
-                    if not paragraph and length > 1:
-                        html.write('<p>\n')
-                        paragraph = True
-                    elif length > 1:
-                        html.write('<br/>\n')
-                    elif paragraph:
-                        html.write('</p>\n')
-                        paragraph = False
-
-                if length > 1:
-                    html.write(line)
-
-            if unordered_start:
-                html.write('</ul>\n')
-            if ordered_start:
-                html.write('</ol>\n')
-            if paragraph:
-                html.write('</p>\n')
-    exit (0)
+    exit(0)
